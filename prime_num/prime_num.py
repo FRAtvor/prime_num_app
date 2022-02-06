35d299e7f0dd9ec3417938d7de5649396d3ac5f1
FRAtvor <97030927+FRAtvor@users.noreply.github.com>
2022-02-06 22:05:25 +0100

    init

---
+++ prime_num/prime_num.py
@@ -0,0 +1,48 @@
+import math
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def prime_num_test(number):
+    """
+    tests if given number is prime number
+    """
+    if number <= 1:
+        return False
+    elif number == 2:
+        return True
+    elif (number > 2) & (number % 2 == 0):  # even numbers, not prime numbers, saves loops
+        return False
+    
+    max_divisior = math.floor(math.sqrt(number))
+    for i in range(3, max_divisior+1, 2):      # <=1, 2, solved in if, max_div + 1 - exclusive behavior of range interval, step 2 - skip even numbers, solved in if statement 
+        if number % i == 0:
+            return False
+    
+    return True
+
+
+def find_nearest_pn(number):
+    """
+    finds the nearest prime number, except given
+    """
+    step=1
+    while True:
+        if prime_num_test(number+step) == True:
+            return number+step
+        elif prime_num_test(number-step) == True:
+            return number+step
+        else:
+            step += 1        
+    
+
+
+
+
+
+
+
+
+
+
