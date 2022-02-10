79d018670d7e836bcfd673156597ea847842d542
FRAtvor <97030927+FRAtvor@users.noreply.github.com>
2022-02-10 21:14:49 +0100

    get_test_num chg

--- prime_num/flask_app.py
+++ prime_num/flask_app.py
@@ -1,17 +1,24 @@
 from flask import Flask, request, url_for
+from prime_num import prime_num_test, find_nearest_pn
+import math
 
 app = Flask(__name__)
 
 
-@app.route('/prime_num', methods=['GET'])
-def get_url():
-    args = request.args.get('x')
-    return args
+@app.route('/prime_test', methods=['GET'])
+def get_test_num():
+    args = request.args.get('x', default=0, type=int)
+    if prime_num_test(args):
+        return "bingo"
+    else:
+        return f"{args} is not a prime number. The nearest prime number is: {find_nearest_pn(args)}"
+   
 
 
 if __name__ == '__main__':
-   app.run(debug= True)
+   app.run(debug=True)
 
-   print(args)
+  
+
     # print(type(args.to_dict()))
     # <class 'dict'>
